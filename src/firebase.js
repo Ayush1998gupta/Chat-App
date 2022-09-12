9d2efee13e7cfd8f55550f279d96b023bdb52338
Ayush <aayush199813@gmail.com>
2022-09-12 16:48:53 +0530

    Chat-App

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+ 
+};
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+const db = app.firestore();
+
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, googleProvider };
+
+export default db;
